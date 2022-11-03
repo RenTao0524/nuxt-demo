7d7ef5a110712eca89552be6261e585ca5a7bf70
Flutter <flutter@Flutter.local>
2022-11-03 11:22:20 +0800

    add nodemon

--- nuxt-app/server/index.ts
+++ nuxt-app/server/index.ts
@@ -3,7 +3,11 @@ import express from 'express'
 const port = 3001
 const app = express()
 
-app.get('/user', (req: any, res: any) => {
+app.get('/', (_req, res) => {
+  res.send('hello world!')
+})
+
+app.get('/user', (_req, res) => {
   res.send('user info')
 })
 
